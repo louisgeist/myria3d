73234361854cd5e7c4f564d02beee2219362adb0
Louis Geist <louisjjgeist@gmail.com>
2026-06-10 17:54:36 +0200

    sbatch

---
+++ scripts/jz/launch_h100.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#SBATCH --output=/lustre/fswork/projects/rech/unv/usi32yh/myria3d/logs/slurm/%j.out
+#SBATCH --error=/lustre/fswork/projects/rech/unv/usi32yh/myria3d/logs/slurm/%j.err
+#SBATCH -A uhn@h100
+#SBATCH -C h100
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=24
+#SBATCH --hint=nomultithread
+
+
+#SBATCH --job-name=myria3d
+#SBATCH --time=16:00:00
+
+set -euo pipefail
+
+JOB_DIR=/lustre/fswork/projects/rech/unv/usi32yh/myria3d/logs/slurm/${SLURM_JOB_ID}
+mkdir -p ${JOB_DIR}
+
+cp $0 ${JOB_DIR}/script.slurm
+
+{
+    echo "Job ID: $SLURM_JOB_ID"
+    echo "Starting job at: $(date)"
+    echo "Running on host: $(hostname)"
+    echo "Working directory: $(pwd)"
+    echo "Python executable: $(which python)"
+    nvidia-smi  # État initial du GPU
+} > ${JOB_DIR}/job_info.log
+
+module purge
+module load arch/h100
+module load cuda/12.1.0
+module load miniforge/24.9.0
+
+conda activate myria3d
+
+
+conda list > ${JOB_DIR}/conda_env.txt
+
+export WANDB_MODE=offline
+cd /lustre/fswork/projects/rech/unv/usi32yh/myria3d
+
+START_TIME=$(date +%s)
+
+
+###  --- Compiled code ---
+# Export path to code compiled for H100
+
+# Pointops
+POINTOPS_PATH=/lustre/fswork/projects/rech/unv/usi32yh/Pointcept/pointops_build_h100/lib/python3.10/site-packages/pointops-1.0-py3.10-linux-x86_64.egg
+
+# Add pointops to PYTHONPATH | not overriden if already set
+export PYTHONPATH="${POINTOPS_PATH}${PYTHONPATH:+:$PYTHONPATH}"
+
+
+### --- Myria3D syntax ---
+python run.py experiment=flair3d_plus/multitask_v12_pointcept_jz
+
+### ------------------------
+
+END_TIME=$(date +%s)
+DURATION=$((END_TIME - START_TIME))
+
+{
+    echo "Job finished at: $(date)"
+    echo "Duration: ${DURATION} seconds"
+    nvidia-smi  # État final du GPU
+} >> ${JOB_DIR}/job_info.log
